3101a392301906fe0f70d2ac69687cb13edfb52c
Daniellizcano <jdaniellizcanoc@gmail.com>
2022-01-21 13:50:13 -0500

    Refrescando

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -5,6 +5,12 @@ const { mostrarMenu } = require('./helper/mensajes');
 const main = async() =>{
     console.log('Hola Mundo');
 
+    let opt='';
+    do{
+
+    }while(opt!=='0');
+
+
     mostrarMenu();
 
 
